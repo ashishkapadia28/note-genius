bf845dc7bbc810a0851a7c060f2779da648cf9ce
Ashish Kapadia <ashishkapadia28@users.noreply.github.com>
2026-01-31 22:53:51 +0530

    fix: use named wildcard for Express 5 compatibility

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/api/status', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get(/.*/, (req, res) => {
+app.get('/:pathMatch(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, './client/dist/index.html'));
 });
 
